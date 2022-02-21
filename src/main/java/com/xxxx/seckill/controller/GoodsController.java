643d20255a975157a5693f584251d330ea7622e3
zkl <1019675918@qq.com>
2022-02-21 15:17:29 +0800

    seckilldemo

---
+++ src/main/java/com/xxxx/seckill/controller/GoodsController.java
@@ -0,0 +1,63 @@
+package com.xxxx.seckill.controller;
+
+import com.xxxx.seckill.pojo.User;
+import com.xxxx.seckill.service.IGoodsService;
+import com.xxxx.seckill.service.IUserService;
+import com.xxxx.seckill.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsController {
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private IGoodsService goodsService;
+
+    //跳转商品列表页
+    @RequestMapping("/toList")
+    public String toList(Model model, User user) {
+        model.addAttribute("user",user);
+        model.addAttribute("goodsList",goodsService.findGoodsVo());
+        return "goodsList";
+    }
+
+    //跳转商品详情页
+    @RequestMapping("/toDetail/{goodsId}")
+    public String toDetail(Model model,User user,@PathVariable Long goodsId){
+        model.addAttribute("user",user);
+        GoodsVo goodsVo=goodsService.findGoodsVoByGoodsId(goodsId);
+        Date startDate=goodsVo.getStartDate();
+        Date endDate=goodsVo.getEndDate();
+        Date nowDate=new Date();
+        //秒杀状态
+        int secKillStatus=0;
+        //秒杀倒计时
+        int remainSeconds=0;
+        //秒杀未开始
+        if(nowDate.before(startDate)){
+            remainSeconds=((int)((startDate.getTime()-nowDate.getTime())/1000));
+        }
+        else if(nowDate.after(endDate)){
+            //秒杀已结束
+            secKillStatus=2;
+            remainSeconds=-1;
+        }
+        else{
+            //秒杀中
+            secKillStatus=1;
+            remainSeconds=0;
+        }
+        model.addAttribute("remainSeconds",remainSeconds);
+        model.addAttribute("secKillStatus",secKillStatus);
+        model.addAttribute("goods",goodsVo);
+        return "goodsDetail";
+    }
+}
---
+++ src/main/java/com/xxxx/seckill/service/IGoodsService.java
@@ -0,0 +1,23 @@
+package com.xxxx.seckill.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.seckill.pojo.Goods;
+import com.xxxx.seckill.vo.GoodsVo;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhoubin
+ * @since 2022-02-16
+ */
+public interface IGoodsService extends IService<Goods> {
+    //获取商品列表
+    List<GoodsVo> findGoodsVo();
+
+    //获取商品详情
+    GoodsVo findGoodsVoByGoodsId(Long goodsId);
+}
